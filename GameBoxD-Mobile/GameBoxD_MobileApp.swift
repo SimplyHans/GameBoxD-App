6ddd5d7b93561c3c79db817fb47745a6d7857160
SimplyHans <hans.casilao@gmail.com>
2026-03-11 16:09:15 -0400

    Initial Commit

---
+++ GameBoxD-Mobile/GameBoxD_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameBoxD_MobileApp.swift
+//  GameBoxD-Mobile
+//
+//  Created by Tech on 2026-03-11.
+//
+
+import SwiftUI
+
+@main
+struct GameBoxD_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
